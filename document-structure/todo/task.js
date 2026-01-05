f07ff778d5c6935bfce338ae0a6c3358f7f3000d
Igor P <igorpiskurev95@gmail.com>
2026-01-05 22:11:46 +0300

    document structure ready 2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,20 +1,16 @@
-document.querySelector('.tasks__add').addEventListener('click',function(){
-    if(document.querySelector('#task__input').value != ''){
-        let div = document.createElement('div')
-        div.className = 'task'
-        let divEle = document.createElement('div')
-        divEle.className = 'task__title'
-        divEle.textContent = document.querySelector('#task__input').value
-        div.appendChild(divEle)
-        let tRemove = document.createElement('a')
-        tRemove.className = 'task__remove'
-        tRemove.href = '#'
-        tRemove.addEventListener('click',function(event){
-            document.querySelector('.tasks__list').removeChild(document.querySelectorAll('.task')[Array.from(document.querySelectorAll('.task__remove')).indexOf(event.currentTarget)])
-        })
-        tRemove.textContent = '×'
-        div.appendChild(tRemove)
-        document.querySelector('.tasks__list').appendChild(div)
+document.querySelector('.tasks__add').addEventListener('click',function(event){
+    event.preventDefault()
+    if(document.querySelector('#task__input').value.trim() != ''){
+        let tasksList = document.querySelector('.tasks__list')
+        
+        tasksList.insertAdjacentHTML('afterbegin', `
+            <div class="task">
+            <div class="task__title">
+                ${document.querySelector('#task__input').value}
+            </div>
+            <a href="#" class="task__remove" onClick={document.querySelector('.tasks__list').removeChild(document.querySelectorAll('.task')[Array.from(document.querySelectorAll('.task__remove')).indexOf(event.currentTarget)]);event.preventDefault()}>&times;</a>
+            </div>
+        `);
         document.querySelector('#task__input').value = ''
     }
 })
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,10 +1,10 @@
 let hints = document.querySelectorAll('.has-tooltip')
 let hint = document.querySelector('.tooltip')
 for(let hint_button of hints){
-    i.addEventListener('click',function(event){
+    hint_button.addEventListener('click',function(event){
         event.preventDefault()
         let rect = event.currentTarget.getBoundingClientRect()
-        if(hint.style.left == Number(rect.x.toFixed(3))+'px' && hint.style.top == Numeber((rect.y+rect.height).toFixed(3))+'px' && hint.classList.contains('tooltip_active')){
+        if(hint.style.left == Number(rect.x.toFixed(3))+'px' && hint.style.top == Number((rect.y+rect.height).toFixed(3))+'px' && hint.classList.contains('tooltip_active')){
             hint.classList.remove('tooltip_active')
         } else{
             hint.classList.add('tooltip_active')
